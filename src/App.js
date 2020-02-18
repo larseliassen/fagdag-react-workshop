a274b4a82719e1b0f599a0477754c99360805598
lars_eliassen <lars@jobbnorge.no>
2020-02-18 22:06:57 +0100

    start here

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import JobList from "./JobList.js";
-import JobListing from "./JobListing.js";
-import Header from "./Header.js"
 
 const App = () => {
   return (     
-    <BrowserRouter>
-      <Header /> 
-      <Route exact path="/">              
-        <JobList />
-      </Route>
-      <Route exact path="/position/:id">
-        <JobListing />
-      </Route>
-    </BrowserRouter>
+    null    
   );
 };
 
